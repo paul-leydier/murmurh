1b1ec42d5f0af771fad121da4f6cf39176b8941a
Paul Leydier <paul.leydier@compass-ft.com>
2023-01-15 10:52:58 +0100

    ♻️declare two interfaces: mmh3_128_x64 and mmh3_128_x64_bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
 use pyo3::prelude::*;
+use pyo3::types::PyBytes;
 
 #[pyfunction]
-fn mmh3_128_x64(key: String, seed: u64) -> PyResult<String> {
-    // Initialization
-    let a = mmh3_128_x64_bytes(key.as_bytes(), seed);
-    Ok(format!("{:x}", a))
+#[pyo3(text_signature = "(key, /, seed = 0)")]
+fn mmh3_128_x64_bytes(py: Python, key: String, seed: u64) -> PyResult<&PyBytes> {
+    let hashed = _mmh3_128_x64(key.as_bytes(), seed).to_le_bytes();
+    Ok(PyBytes::new(py, hashed.as_slice()))
 }
 
-fn mmh3_128_x64_bytes(bytes: &[u8], seed: u64) -> u128 {
+#[pyfunction]
+#[pyo3(text_signature = "(key, /, seed = 0)")]
+fn mmh3_128_x64(key: String, seed: u64) -> PyResult<String> {
+    let hashed = _mmh3_128_x64(key.as_bytes(), seed);
+    Ok(format!("{:x}", hashed))
+}
+
+fn _mmh3_128_x64(bytes: &[u8], seed: u64) -> u128 {
     const C1: u64 = 0x87c37b91114253d5;
     const C2: u64 = 0x4cf5ad432745937f;
     const C3: u64 = 0x52dce729;
@@ -84,6 +92,7 @@ fn fmix64(mut k: u64) -> u64 {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn murmurh(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(mmh3_128_x64_bytes, m)?)?;
     m.add_function(wrap_pyfunction!(mmh3_128_x64, m)?)?;
     Ok(())
 }
@@ -94,44 +103,43 @@ mod test {
 
     #[test]
     fn test_empty_string() {
-        let result = mmh3_128_x64_bytes("".as_bytes(), 0);
+        let result = _mmh3_128_x64("".as_bytes(), 0);
         assert_eq!(result, 0);
     }
 
     #[test]
     fn test_16chars() {
-        let result = mmh3_128_x64_bytes("hello world!1234".as_bytes(), 0);
+        let result = _mmh3_128_x64("hello world!1234".as_bytes(), 0);
         assert_eq!(result, 240867822025444006610977441818626719586);
     }
 
     #[test]
     fn test_24chars() {
-        let result = mmh3_128_x64_bytes("hello world!123456789012".as_bytes(), 0);
+        let result = _mmh3_128_x64("hello world!123456789012".as_bytes(), 0);
         assert_eq!(result, 237085087629596244627904512008431960258);
     }
 
     #[test]
     fn test_foo() {
-        let result = mmh3_128_x64_bytes("foo".as_bytes(), 0);
+        let result = _mmh3_128_x64("foo".as_bytes(), 0);
         assert_eq!(result, 168394135621993849475852668931176482145);
     }
 
     #[test]
     fn test_seed() {
-        let result = mmh3_128_x64_bytes("foo".as_bytes(), 42);
+        let result = _mmh3_128_x64("foo".as_bytes(), 42);
         assert_eq!(result, 215966891540331383248189432718888555506);
     }
 
     #[test]
     fn test_fox() {
-        let result =
-            mmh3_128_x64_bytes("The quick brown fox jumps over the lazy dog.".as_bytes(), 0);
+        let result = _mmh3_128_x64("The quick brown fox jumps over the lazy dog.".as_bytes(), 0);
         assert_eq!(result, 140055101589960098446263325149249471177);
     }
 
     #[test]
     fn test_lorem() {
-        let result = mmh3_128_x64_bytes("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".as_bytes(), 0);
+        let result = _mmh3_128_x64("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".as_bytes(), 0);
         assert_eq!(result, 317940214952246513203822842115116886626);
     }
 }

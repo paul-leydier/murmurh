f8df7cbfb86828f1678f96089e2c203fbd18d25d
Paul Leydier <paul.leydier@compass-ft.com>
2023-01-15 11:19:29 +0100

    ✅ unit test default seed

--- murmurh/test_murmurh.py
+++ murmurh/test_murmurh.py
@@ -12,6 +12,11 @@ def test_mmh3_128_x64(test_input: str, expected: str):
     assert murmurh.mmh3_128_x64(test_input, 0) == expected
 
 
+def test_mmh3_128_x64_default_seed():
+    # Correspond to seed 0
+    assert murmurh.mmh3_128_x64("foo") == 168394135621993849475852668931176482145
+
+
 @pytest.mark.parametrize(
     "test_input,expected",
     [

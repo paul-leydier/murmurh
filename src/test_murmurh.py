1d72e656e9c55dbd28464ea99fd0fc7a82ad8cf6
Paul Leydier <paul.leydier@compass-ft.com>
2023-01-15 10:53:14 +0100

    ✅ test the bytes interface

--- src/test_murmurh.py
+++ src/test_murmurh.py
@@ -10,3 +10,13 @@ import pytest
 )
 def test_mmh3_128_x64(test_input: str, expected: str):
     assert murmurh.mmh3_128_x64(test_input, 0) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("", b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"),
+    ],
+)
+def test_mmh3_128_x64_bytes(test_input: str, expected: bytes):
+    assert murmurh.mmh3_128_x64_bytes(test_input, 0) == expected

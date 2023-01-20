6e645d07f1a6cae4e7e3d2ec2ca0eb5b1984d92b
Paul Leydier <paul.leydier@compass-ft.com>
2023-01-20 11:51:24 +0100

    ♻️ route exposed functions using a single non-exposed function

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,33 @@ use pyo3::types::PyBytes;
 
 #[pyfunction]
 #[pyo3(text_signature = "(key, /, seed = 0, arch = \"x64\", size = 128)")]
-fn hash(key: String, seed: Option<u64>) -> u128 {
-    x64::hash_128(key.as_bytes(), seed.unwrap_or_default())
+fn hash(key: String, seed: Option<u64>, arch: Option<String>, size: Option<u8>) -> u128 {
+    route_hash(key, seed, arch, size)
 }
 
 #[pyfunction]
 #[pyo3(text_signature = "(key, /, seed = 0, arch = \"x64\", size = 128)")]
-fn hash_bytes(py: Python, key: String, seed: Option<u64>) -> &PyBytes {
-    let hashed = x64::hash_128(key.as_bytes(), seed.unwrap_or_default()).to_le_bytes();
+fn hash_bytes(
+    py: Python,
+    key: String,
+    seed: Option<u64>,
+    arch: Option<String>,
+    size: Option<u8>,
+) -> &PyBytes {
+    let hashed = route_hash(key, seed, arch, size).to_le_bytes();
     PyBytes::new(py, hashed.as_slice())
 }
 
 #[pyfunction]
 #[pyo3(text_signature = "(key, /, seed = 0, arch = \"x64\", size = 128)")]
-fn hash_hex(key: String, seed: Option<u64>) -> String {
-    let hashed = x64::hash_128(key.as_bytes(), seed.unwrap_or_default());
+fn hash_hex(key: String, seed: Option<u64>, arch: Option<String>, size: Option<u8>) -> String {
+    let hashed = route_hash(key, seed, arch, size);
     format!("{:x}", hashed)
+}
+
+#[inline]
+fn route_hash(key: String, seed: Option<u64>, arch: Option<String>, size: Option<u8>) -> u128 {
+    x64::hash_128(key.as_bytes(), seed.unwrap_or_default())
 }
 
 /// A Python module implemented in Rust.

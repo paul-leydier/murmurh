b560eeaf71c9a09ec9cb7477363593e19f3f885c
Paul Leydier <paul.leydier@compass-ft.com>
2023-01-20 12:30:27 +0100

    🥅 raise errors related to arguments provided

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 mod x64;
 
+use pyo3::exceptions;
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 
 #[pyfunction]
 #[pyo3(text_signature = "(key, /, seed = 0, arch = \"x64\", size = 128)")]
-fn hash(key: String, seed: Option<u64>, arch: Option<&str>, size: Option<u8>) -> u128 {
-    route_hash(key, seed, arch, size).expect("error")
+fn hash(key: String, seed: Option<u64>, arch: Option<&str>, size: Option<u8>) -> PyResult<u128> {
+    route_hash(key, seed, arch, size)
 }
 
 #[pyfunction]
@@ -17,18 +18,21 @@ fn hash_bytes<'a>(
     seed: Option<u64>,
     arch: Option<&str>,
     size: Option<u8>,
-) -> &'a PyBytes {
-    let hashed = route_hash(key, seed, arch, size)
-        .expect("error")
-        .to_le_bytes();
-    PyBytes::new(py, hashed.as_slice())
+) -> PyResult<&'a PyBytes> {
+    let hashed = route_hash(key, seed, arch, size)?.to_le_bytes();
+    Ok(PyBytes::new(py, hashed.as_slice()))
 }
 
 #[pyfunction]
 #[pyo3(text_signature = "(key, /, seed = 0, arch = \"x64\", size = 128)")]
-fn hash_hex(key: String, seed: Option<u64>, arch: Option<&str>, size: Option<u8>) -> String {
-    let hashed = route_hash(key, seed, arch, size).expect("error");
-    format!("{:x}", hashed)
+fn hash_hex(
+    key: String,
+    seed: Option<u64>,
+    arch: Option<&str>,
+    size: Option<u8>,
+) -> PyResult<String> {
+    let hashed = route_hash(key, seed, arch, size)?;
+    Ok(format!("{:x}", hashed))
 }
 
 #[inline]
@@ -37,13 +41,13 @@ fn route_hash(
     seed: Option<u64>,
     arch: Option<&str>,
     size: Option<u8>,
-) -> Result<u128, &str> {
+) -> PyResult<u128> {
     match (arch.unwrap_or("x64"), size.unwrap_or(128)) {
         ("x64", 128) => Ok(x64::hash_128(key.as_bytes(), seed.unwrap_or_default())),
-        ("x64", 64) => Err("Not Implemented."),
-        ("x86", 128) => Err("Not Implemented."),
-        ("x86", 64) => Err("Not Implemented."),
-        _ => Err("Invalid input."),
+        ("x64", 64) => Err(exceptions::PyValueError::new_err("Not Implemented yet.")),
+        ("x86", 128) => Err(exceptions::PyValueError::new_err("Not Implemented yet.")),
+        ("x86", 64) => Err(exceptions::PyValueError::new_err("Not Implemented yet.")),
+        _ => Err(exceptions::PyValueError::new_err("Invalid input.")),
     }
 }
 

29e7d290dcd8cb3358a489e7e7d9fce892abfc6e
Paul Leydier <paul.leydier@compass-ft.com>
2023-01-20 11:41:33 +0100

    🚚 extract x64 functions to dedicated file

--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,26 @@
+mod x64;
+
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 
 #[pyfunction]
 #[pyo3(text_signature = "(key, /, seed = 0, arch = \"x64\", size = 128)")]
 fn hash(key: String, seed: Option<u64>) -> u128 {
-    _mmh3_128_x64(key.as_bytes(), seed.unwrap_or_default())
+    x64::hash_128(key.as_bytes(), seed.unwrap_or_default())
 }
 
 #[pyfunction]
 #[pyo3(text_signature = "(key, /, seed = 0, arch = \"x64\", size = 128)")]
 fn hash_bytes(py: Python, key: String, seed: Option<u64>) -> &PyBytes {
-    let hashed = _mmh3_128_x64(key.as_bytes(), seed.unwrap_or_default()).to_le_bytes();
+    let hashed = x64::hash_128(key.as_bytes(), seed.unwrap_or_default()).to_le_bytes();
     PyBytes::new(py, hashed.as_slice())
 }
 
 #[pyfunction]
 #[pyo3(text_signature = "(key, /, seed = 0, arch = \"x64\", size = 128)")]
 fn hash_hex(key: String, seed: Option<u64>) -> String {
-    let hashed = _mmh3_128_x64(key.as_bytes(), seed.unwrap_or_default());
+    let hashed = x64::hash_128(key.as_bytes(), seed.unwrap_or_default());
     format!("{:x}", hashed)
-}
-
-fn _mmh3_128_x64(bytes: &[u8], seed: u64) -> u128 {
-    const C1: u64 = 0x87c37b91114253d5;
-    const C2: u64 = 0x4cf5ad432745937f;
-    const C3: u64 = 0x52dce729;
-    const C4: u64 = 0x38495ab5;
-    let len = bytes.len();
-    let mut h1 = seed;
-    let mut h2 = seed;
-
-    // Body
-    for chunk in bytes.chunks_exact(16) {
-        let k1 = u64::from_le_bytes(chunk[0..8].try_into().expect("oups"));
-        let k2 = u64::from_le_bytes(chunk[8..16].try_into().expect("oups"));
-        h1 ^= k1.wrapping_mul(C1).rotate_left(31).wrapping_mul(C2);
-        h1 = h1
-            .rotate_left(27)
-            .wrapping_add(h2)
-            .wrapping_mul(5)
-            .wrapping_add(C3);
-        h2 ^= k2.wrapping_mul(C2).rotate_left(33).wrapping_mul(C1);
-        h2 = h2
-            .rotate_left(31)
-            .wrapping_add(h1)
-            .wrapping_mul(5)
-            .wrapping_add(C4);
-    }
-
-    // Tail
-    let mut remainder = len % 16;
-    let cursor = len - remainder;
-    if remainder > 0 {
-        if remainder > 8 {
-            let mut tail: [u8; 8] = [0; 8];
-            for (i, b) in bytes[cursor + 8..cursor + remainder].iter().enumerate() {
-                tail[i] = *b;
-            }
-            let k2 = u64::from_le_bytes(tail);
-            remainder = 8;
-            h2 ^= k2.wrapping_mul(C2).rotate_left(33).wrapping_mul(C1);
-        }
-        let mut tail: [u8; 8] = [0; 8];
-        for (i, b) in bytes[cursor..cursor + remainder].iter().enumerate() {
-            tail[i] = *b;
-        }
-        let k1 = u64::from_le_bytes(tail);
-        h1 ^= k1.wrapping_mul(C1).rotate_left(31).wrapping_mul(C2);
-    }
-
-    h1 ^= len as u64;
-    h2 ^= len as u64;
-    h1 = h1.wrapping_add(h2);
-    h2 = h2.wrapping_add(h1);
-
-    h1 = fmix64(h1);
-    h2 = fmix64(h2);
-
-    h1 = h1.wrapping_add(h2);
-    h2 = h2.wrapping_add(h1);
-
-    (h2 as u128) << 64 | h1 as u128
-}
-
-#[inline]
-fn fmix64(mut k: u64) -> u64 {
-    const C5: u64 = 0xff51afd7ed558ccd;
-    const C6: u64 = 0xc4ceb9fe1a85ec53;
-    k ^= k >> 33;
-    k = k.wrapping_mul(C5);
-    k ^= k >> 33;
-    k = k.wrapping_mul(C6);
-    k ^= k >> 33;
-    k
 }
 
 /// A Python module implemented in Rust.
@@ -102,51 +30,4 @@ fn murmurh(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hash_bytes, m)?)?;
     m.add_function(wrap_pyfunction!(hash_hex, m)?)?;
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_empty_string() {
-        let result = _mmh3_128_x64("".as_bytes(), 0);
-        assert_eq!(result, 0);
-    }
-
-    #[test]
-    fn test_16chars() {
-        let result = _mmh3_128_x64("hello world!1234".as_bytes(), 0);
-        assert_eq!(result, 240867822025444006610977441818626719586);
-    }
-
-    #[test]
-    fn test_24chars() {
-        let result = _mmh3_128_x64("hello world!123456789012".as_bytes(), 0);
-        assert_eq!(result, 237085087629596244627904512008431960258);
-    }
-
-    #[test]
-    fn test_foo() {
-        let result = _mmh3_128_x64("foo".as_bytes(), 0);
-        assert_eq!(result, 168394135621993849475852668931176482145);
-    }
-
-    #[test]
-    fn test_seed() {
-        let result = _mmh3_128_x64("foo".as_bytes(), 42);
-        assert_eq!(result, 215966891540331383248189432718888555506);
-    }
-
-    #[test]
-    fn test_fox() {
-        let result = _mmh3_128_x64("The quick brown fox jumps over the lazy dog.".as_bytes(), 0);
-        assert_eq!(result, 140055101589960098446263325149249471177);
-    }
-
-    #[test]
-    fn test_lorem() {
-        let result = _mmh3_128_x64("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".as_bytes(), 0);
-        assert_eq!(result, 317940214952246513203822842115116886626);
-    }
 }
---
+++ src/x64.rs
@@ -0,0 +1,122 @@
+use std::convert::TryInto;
+
+#[inline]
+fn fmix64(mut k: u64) -> u64 {
+    const C5: u64 = 0xff51afd7ed558ccd;
+    const C6: u64 = 0xc4ceb9fe1a85ec53;
+    k ^= k >> 33;
+    k = k.wrapping_mul(C5);
+    k ^= k >> 33;
+    k = k.wrapping_mul(C6);
+    k ^= k >> 33;
+    k
+}
+
+pub fn hash_128(bytes: &[u8], seed: u64) -> u128 {
+    const C1: u64 = 0x87c37b91114253d5;
+    const C2: u64 = 0x4cf5ad432745937f;
+    const C3: u64 = 0x52dce729;
+    const C4: u64 = 0x38495ab5;
+    let len = bytes.len();
+    let mut h1 = seed;
+    let mut h2 = seed;
+
+    // Body
+    for chunk in bytes.chunks_exact(16) {
+        let k1 = u64::from_le_bytes(chunk[0..8].try_into().expect("oups"));
+        let k2 = u64::from_le_bytes(chunk[8..16].try_into().expect("oups"));
+        h1 ^= k1.wrapping_mul(C1).rotate_left(31).wrapping_mul(C2);
+        h1 = h1
+            .rotate_left(27)
+            .wrapping_add(h2)
+            .wrapping_mul(5)
+            .wrapping_add(C3);
+        h2 ^= k2.wrapping_mul(C2).rotate_left(33).wrapping_mul(C1);
+        h2 = h2
+            .rotate_left(31)
+            .wrapping_add(h1)
+            .wrapping_mul(5)
+            .wrapping_add(C4);
+    }
+
+    // Tail
+    let mut remainder = len % 16;
+    let cursor = len - remainder;
+    if remainder > 0 {
+        if remainder > 8 {
+            let mut tail: [u8; 8] = [0; 8];
+            for (i, b) in bytes[cursor + 8..cursor + remainder].iter().enumerate() {
+                tail[i] = *b;
+            }
+            let k2 = u64::from_le_bytes(tail);
+            remainder = 8;
+            h2 ^= k2.wrapping_mul(C2).rotate_left(33).wrapping_mul(C1);
+        }
+        let mut tail: [u8; 8] = [0; 8];
+        for (i, b) in bytes[cursor..cursor + remainder].iter().enumerate() {
+            tail[i] = *b;
+        }
+        let k1 = u64::from_le_bytes(tail);
+        h1 ^= k1.wrapping_mul(C1).rotate_left(31).wrapping_mul(C2);
+    }
+
+    h1 ^= len as u64;
+    h2 ^= len as u64;
+    h1 = h1.wrapping_add(h2);
+    h2 = h2.wrapping_add(h1);
+
+    h1 = fmix64(h1);
+    h2 = fmix64(h2);
+
+    h1 = h1.wrapping_add(h2);
+    h2 = h2.wrapping_add(h1);
+
+    (h2 as u128) << 64 | h1 as u128
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_empty_string() {
+        let result = hash_128("".as_bytes(), 0);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_16chars() {
+        let result = hash_128("hello world!1234".as_bytes(), 0);
+        assert_eq!(result, 240867822025444006610977441818626719586);
+    }
+
+    #[test]
+    fn test_24chars() {
+        let result = hash_128("hello world!123456789012".as_bytes(), 0);
+        assert_eq!(result, 237085087629596244627904512008431960258);
+    }
+
+    #[test]
+    fn test_foo() {
+        let result = hash_128("foo".as_bytes(), 0);
+        assert_eq!(result, 168394135621993849475852668931176482145);
+    }
+
+    #[test]
+    fn test_seed() {
+        let result = hash_128("foo".as_bytes(), 42);
+        assert_eq!(result, 215966891540331383248189432718888555506);
+    }
+
+    #[test]
+    fn test_fox() {
+        let result = hash_128("The quick brown fox jumps over the lazy dog.".as_bytes(), 0);
+        assert_eq!(result, 140055101589960098446263325149249471177);
+    }
+
+    #[test]
+    fn test_lorem() {
+        let result = hash_128("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".as_bytes(), 0);
+        assert_eq!(result, 317940214952246513203822842115116886626);
+    }
+}

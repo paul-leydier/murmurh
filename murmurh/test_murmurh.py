38442af1df53e085bf2afca015dad1ccaacaaa4b
Paul Leydier <paul.leydier@compass-ft.com>
2023-01-24 22:24:36 +0100

    🚚 reorder tests

--- murmurh/test_murmurh.py
+++ murmurh/test_murmurh.py
@@ -2,16 +2,6 @@ import murmurh
 import pytest
 
 
-@pytest.mark.parametrize(
-    "test_input,expected",
-    [
-        ("", 0),
-    ],
-)
-def test_mmh3_128_x64(test_input: str, expected: str):
-    assert murmurh.hash(test_input, 0) == expected
-
-
 def test_mmh3_128_x64_default_seed():
     # Correspond to seed 0
     assert murmurh.hash("foo", arch="x64", size=128) == 168394135621993849475852668931176482145

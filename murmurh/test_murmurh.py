eb179665e18bf73e016eee0ceb3b001f8b2fa97f
Paul Leydier <paul.leydier@compass-ft.com>
2023-01-24 22:25:16 +0100

    🎨 apply black

--- murmurh/test_murmurh.py
+++ murmurh/test_murmurh.py
@@ -4,17 +4,25 @@ import pytest
 
 def test_mmh3_128_x64_default_seed():
     # Correspond to seed 0
-    assert murmurh.hash("foo", arch="x64", size=128) == 168394135621993849475852668931176482145
+    assert (
+        murmurh.hash("foo", arch="x64", size=128)
+        == 168394135621993849475852668931176482145
+    )
 
 
 def test_mmh3_128_x64_default_arch():
     # Correspond to arch "x64"
-    assert murmurh.hash("foo", seed=0, size=128) == 168394135621993849475852668931176482145
+    assert (
+        murmurh.hash("foo", seed=0, size=128) == 168394135621993849475852668931176482145
+    )
 
 
 def test_mmh3_128_x64_default_size():
     # Correspond to size 128
-    assert murmurh.hash("foo", seed=0, arch="x64") == 168394135621993849475852668931176482145
+    assert (
+        murmurh.hash("foo", seed=0, arch="x64")
+        == 168394135621993849475852668931176482145
+    )
 
 
 @pytest.mark.parametrize(
